af14ac17280a8085d47ba5775b1f2d7f9eafe4e2
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-16 13:50:29 -0500

    Copied TCPserver.cpp and TCPclient.cpp to UDPserver.cpp and UDPclient.cpp. Intend on creating a UDP client and server model with newly created files

---
+++ UDPclient.cpp
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h> //included this for the close function
+
+
+#include <netinet/in.h> //contains socket structs
+
+int main(int argc, char *argv[])
+{
+
+	// creating client socket
+	int network_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	// integer to hold port # from command line argument
+	// want to take second string argument to use as port #
+	// check argc
+	const static int port_number = atoi(argv[1]);
+
+	// specify an address for the socket to connect to
+	::sockaddr_in server_address;
+	auto serveraddr_ptr = &server_address;
+
+	// need to define a few fields within the sockaddr_in struct
+	// what is the family
+	server_address.sin_family = AF_INET;
+	// specify port
+	// htons = host to network (making sure data format is correct)
+	// puts integer port into correct byte order
+	// pass the actual port, htons takes care of conversion
+	server_address.sin_port = htons(port_number);
+
+	// specify actual address
+	// server_address is a struct with a sin_addr struct member which contains
+	// s_addr field
+	// conecting to loval machine, can use any address connected to local machine
+	server_address.sin_addr.s_addr = INADDR_ANY;
+
+	// first arg is actual socket
+	// 2nd param, need to cast pointer  to struct sockaddr
+	// 3rd param is size of address
+	int connection_status = ::connect(network_socket, (struct sockaddr *)serveraddr_ptr, sizeof(server_address));
+
+	// check for error with connection. if connect = -1, means error connecting
+	if (connection_status == -1)
+	{
+		printf("There was an error making a connection to the remote socket\n");
+	}
+	else 
+	{
+		// recieve data from the server using recv function
+		char server_response[14]; // hold message that we get back from server
+		int recieve_status = ::recv(network_socket, &server_response, sizeof(server_response), 0);
+
+		if(recieve_status == 14) {       //checking if the server message fills server_response
+			printf("The server message has filled up the buffer: %s \n", server_response);
+			printf("Recieve Status: %u \n", recieve_status);
+		}
+		else {
+			printf("The server sent the data: %s \n", server_response);
+			printf("Recieve Status: %u \n", recieve_status);
+		}
+
+		// and then close the socket
+		::close(network_socket);
+	}
+	return 0;
+}
---
+++ UDPserver.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+#include <unistd.h> //included this for the close function
+
+#include <netinet/in.h>
+
+    // accept port number as command line argument
+   //  const static int default_port_number = 9002;
+   // static int port_number = default_port_number;
+
+int main(int argc, char *argv[])
+{
+
+    const static int port_number = atoi(argv[1]);
+
+    // data to be sent to cleints
+    char server_message[20] = "Recieved messagewww";
+
+    
+
+    // make socket call to get socket file descriptor
+    const int server_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+    // define the server address
+    ::sockaddr_in server_address;
+    auto serveraddr_ptr = &server_address;
+    // specify family (AF_INET)
+    server_address.sin_family = AF_INET;
+
+    // specify port
+    server_address.sin_port = htons(port_number);
+
+    // specify address
+    // INADDR_ANY resolves to any IP address on local machine
+
+    server_address.sin_addr.s_addr = INADDR_ANY;
+
+    // need to bind server socket to specified IP address and port
+    // first argument is socket file descriptor
+    // second argument is the port number
+    // third is size of server address
+    // ::-->scoping operating
+    //change 2nd argument to c++ cast
+    ::bind(server_socket, (sockaddr *)serveraddr_ptr, sizeof(server_address));
+
+    // now that the server is bound to an address, we can enable server to listen
+    // for connections from the clients
+    // first argument is socket file descriptor
+    // 2nd is backlog, how many connections can be waiting for this socket
+    ::listen(server_socket, 5);
+
+    // integer to hold client socket, when we listen for connections,
+    // can begin to accept connections, accept call gives back client socket fd
+
+    // first parameter is socket we are accepting connections on, aka server socket
+    // next two params can be left as NULL for now
+    int client_socket = ::accept(server_socket, NULL, NULL);
+
+    // we can send data to client
+    // first arg is socket we are sending data to
+    // second arg is data we want to send
+    // third argument is size of message
+    ::send(client_socket, server_message, sizeof(server_message), NULL);
+
+    // can close socket
+    ::close(server_socket);
+    return 0;
+}
