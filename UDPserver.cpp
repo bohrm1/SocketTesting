0155802cd61226de7c12ae295f8858a73dcf442c
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-20 16:19:32 -0500

    Changed MAXLINE macro that was used to set the buffer size to a gloabl constant integer since macros should be avoided when possible for the most part. Also implemented gflags so that the port number can be specified using flags as opposed to reading the second argument on the command line, a more robust way of specifying command line arguments. Added port validation tha  checks if the port number is in a certain range, along with some other gflag commands.

--- UDPserver.cpp
+++ UDPserver.cpp
@@ -9,15 +9,34 @@
 #include <unistd.h> //included this for the close function
 
 #include <netinet/in.h>
-#define MAXLINE 1024
+#include "gflags/gflags.h"
+constexpr int MAXLINE = 1024;
 
 
+
+static bool ValidatePort(const char* flagname, int32_t value) {
+   if (value > 0 && value < 32768)   // value is ok
+     return true;
+   printf("Invalid value for --%s: %d\n", flagname, (int)value);
+   return false;
+}
+
+DEFINE_int32(port_number, 9002, "What port to listen on");
+DEFINE_validator(port_number, &ValidatePort);
 
 int main(int argc, char *argv[])
 {
-    const static int port_number = atoi(argv[1]);
+    gflags::SetVersionString("1.0");
+
+    gflags::SetUsageMessage(
+      "port_number"); 
+
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+    //const static int port_number = atoi(argv[1]);
+    
+    printf("Listening on port: %d \n", FLAGS_port_number);
     char *server_message = "Server message";
-    char buffer[MAXLINE];
+    char buffer[MAXLINE]; //take out macro 
 
     const int server_socket = socket(AF_INET, SOCK_DGRAM, 0);
     // make socket call to get socket file descriptor
@@ -32,7 +51,7 @@ int main(int argc, char *argv[])
     auto clientaddr_ptr = &client_address;
     
     server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(port_number);
+    server_address.sin_port = htons(FLAGS_port_number);
     server_address.sin_addr.s_addr = INADDR_ANY;
 
  
@@ -53,5 +72,6 @@ int main(int argc, char *argv[])
     
     ::sendto(server_socket, (const char *)server_message, strlen(server_message), MSG_CONFIRM, (const struct sockaddr *)clientaddr_ptr, client_address_length);
 
+    gflags::ShutDownCommandLineFlags();
     return 0;
 }
