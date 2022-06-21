08dc96015b653445aa9dc9ed510141a600d3d6db
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-21 10:47:43 -0500

    Created a main.cpp file that will be used to instantiate the TCPclient object.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include<string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h> //included this for the close function
+
+#include <netinet/in.h> //contains socket structs
+#include "gflags/gflags.h"
+
+#include "TCPclient.h"
+
+
+int main (int argc, char *argv[])  {
+
+
+
+    return 0;
+}
