f28115fe5974fd46fffcefb74eba7a8934b99480
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-21 15:28:52 -0500

    Implemented gflags so that the port number can be passed in using the command line for TCPclientmain.cpp.

--- TCPclientmain.cpp
+++ TCPclientmain.cpp
@@ -12,10 +12,25 @@
 
 #include "TCPclient.h"
 
+static bool ValidatePort(const char* flagname, int32_t value) {
+   if (value > 0 && value < 32768)   // value is ok
+     return true;
+   printf("Invalid value for --%s: %d\n", flagname, (int)value);
+   return false;
+}
+
+DEFINE_int32(port_number, 9002, "What port to listen on");
+DEFINE_validator(port_number, &ValidatePort);
 
 int main (int argc, char *argv[])  {
 
-    TCPclient client1(9002, "example_address");
+    gflags::SetVersionString("1.0");
+	gflags::SetUsageMessage ("port_number");
+    gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+    printf("Listening on port: %d \n", FLAGS_port_number);
+
+    TCPclient client1(FLAGS_port_number, "example_address");
 
     return 0;
 }
