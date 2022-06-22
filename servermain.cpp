9a2a58c9146853ea13f9bf831a99fd76156f5391
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-22 15:02:36 -0500

    Introduced a gflag called comms_protocol which will be passed into the Server object in order to specify what type of communication will be used on the server's end.

--- servermain.cpp
+++ servermain.cpp
@@ -10,7 +10,7 @@
 #include <netinet/in.h> //contains socket structs
 #include "gflags/gflags.h"
 
-#include "TCPserver.h"
+#include "server.h"
 
 
 static bool ValidatePort(const char* flagname, int32_t value) {
@@ -23,16 +23,21 @@ static bool ValidatePort(const char* flagname, int32_t value) {
 DEFINE_int32(port_number, 9002, "What port to listen on");
 DEFINE_validator(port_number, &ValidatePort);
 
+DEFINE_string(comms_protocol, "TCP", "Communication protocol (TCP/UDP). Defualt is TCP");
+//DEFINE_validator(port_number, &ValidatePort);
+
 int main (int argc, char *argv[])  {
 
 
     gflags::SetVersionString("1.0");
-	gflags::SetUsageMessage ("port_number");
+	  gflags::SetUsageMessage ("port_number" "comms protocol");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     printf("Listening on port: %d \n", FLAGS_port_number);
+    printf("Communication protocol: %s \n", FLAGS_comms_protocol);
 
-    TCPserver server1(FLAGS_port_number, "example_address");
+    Server server1(FLAGS_port_number, "example_address", FLAGS_comms_protocol);
+    
 
     gflags::ShutDownCommandLineFlags();
     return 0;
