ebb0f531fc371cb18432bfb777a1c4d51dc38153
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-22 14:08:44 -0500

    Created general server files that will be needed to build the server class which will be able to communicate with the client class using either UDP or TCP.

---
+++ server.h
@@ -0,0 +1,25 @@
+#ifndef TCPCLIENT_H
+#define TCPCLIENT_h
+
+#include <string>
+
+class TCPserver 
+{
+private:
+    int PortNumber;
+    std::string ServerAddr;
+    int ServerSocket; 
+
+public: 
+    TCPserver(int port_number, std::string server_addr);
+    ~TCPserver();
+
+    void setPortNumber(int port_number);
+    int getPortNumber(void);
+    void setServerAddress(std::string server_addr);
+    std::string getServerAddress(void);
+
+
+};
+
+#endif
