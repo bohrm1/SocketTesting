b1080fb45d6fc3897e1b4fc114c7f7cc394856c0
Matthew Bohr <bohrm1@tcnj.edu>
2022-06-22 11:48:39 -0500

    Added gflag for the communication protocol. User will specify either UDP or TCP in command line.

--- clientmain.cpp
+++ clientmain.cpp
@@ -10,7 +10,7 @@
 #include <netinet/in.h> //contains socket structs
 #include "gflags/gflags.h"
 
-#include "TCPclient.h"
+#include "client.h"
 
 static bool ValidatePort(const char* flagname, int32_t value) {
    if (value > 0 && value < 32768)   // value is ok
@@ -22,15 +22,19 @@ static bool ValidatePort(const char* flagname, int32_t value) {
 DEFINE_int32(port_number, 9002, "What port to listen on");
 DEFINE_validator(port_number, &ValidatePort);
 
+DEFINE_string(comms_protocol, "TCP", "Communication protocol (TCP/UDP). Defualt is TCP");
+//DEFINE_validator(port_number, &ValidatePort);
+
 int main (int argc, char *argv[])  {
 
     gflags::SetVersionString("1.0");
-	  gflags::SetUsageMessage ("port_number");
+	  gflags::SetUsageMessage ("port_number" "comm_protocol");
     gflags::ParseCommandLineFlags(&argc, &argv, true);
 
     printf("Listening on port: %d \n", FLAGS_port_number);
+    printf("Communication protocol: %s \n", FLAGS_comms_protocol);
 
-    TCPclient client1(FLAGS_port_number, "example_address");
+    Client client1(FLAGS_port_number, "example_address", FLAGS_comms_protocol);
 
     return 0;
 }
