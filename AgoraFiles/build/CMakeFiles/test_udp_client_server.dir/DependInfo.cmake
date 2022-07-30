7986e5c8c9377a2875985b1362c3af7fe5f6c8ff
Matthew Bohr <bohrm1@tcnj.edu>
2022-07-30 12:22:52 -0500

    Deleting repeat files.

---
+++ AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/space/mjb20/socket/AgoraFiles/test_udp_client_server.cc" "/space/mjb20/socket/AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/test_udp_client_server.cc.o"
+  "/space/mjb20/socket/AgoraFiles/udp_client.cc" "/space/mjb20/socket/AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/udp_client.cc.o"
+  "/space/mjb20/socket/AgoraFiles/udp_server_ipv6.cc" "/space/mjb20/socket/AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/udp_server_ipv6.cc.o"
+  "/space/mjb20/socket/AgoraFiles/utils.cc" "/space/mjb20/socket/AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/utils.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "GFLAGS_IS_A_DLL=0"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ AgoraFiles/build/CMakeFiles/test_udp_client_server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_udp_client_server.dir/test_udp_client_server.cc.o"
+  "CMakeFiles/test_udp_client_server.dir/udp_client.cc.o"
+  "CMakeFiles/test_udp_client_server.dir/udp_server_ipv6.cc.o"
+  "CMakeFiles/test_udp_client_server.dir/utils.cc.o"
+  "test_udp_client_server"
+  "test_udp_client_server.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_udp_client_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
